0582af6719e2a7952ce2455675398f8397f92b37
Nitsan Avni <nitsanav@gmail.com>
2023-09-02 13:19:03 +0000

    minor

--- repro.sh
+++ repro.sh
@@ -3,6 +3,6 @@ export PATH=$BUN_INSTALL/bin:$PATH
 
 bun --version || (curl -fsSL https://bun.sh/install | bash)
 
-# bun test process never terminates on its own so we timeout
+# bun test process never terminates on its own so we timeout - can't reproduce issue 1605 like that
 timeout 5 bun test
 
